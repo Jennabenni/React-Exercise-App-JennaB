79dae6fc5bb86c06c2b9cd383b75b937e0eb514c
Jennabenni <je686804@ucf.edu>
2025-02-17 17:34:36 -0500

    commit

--- exercise-app/src/components/RepetitionExercise/index.js
+++ exercise-app/src/components/RepetitionExercise/index.js
@@ -2,19 +2,39 @@
 Start at 0
 
 needs to display exercise at top, it says in component, must pass in the name prop*/
+
+
+
+//I understand and shall now explain for myself
+
 import React, {useState} from "react";
+//useState is a hook, something already created
 
 function RepetitionExercise({name}){
+  //runs name prop
 
+
+  //const usually makes arrays or variables
     const [lapCount, setLapCount] = useState(0);
+    //lapCount is a name, setLapCount is a FUNCTION
+    //sets the lap counter at zero
 
+    //this will make it bigger, with button
     const handleIncrement = () => {
+      //prevCount is a parameter, is previous state value
+      //provided by REACT when you use an UPDATER FUNCTION (useState) with
+      //setLapCount
+
+      //setLapCount is a function
       setLapCount((prevCount) => prevCount + 1);
     };
+
+    //reset button, sets value to zero
     const handleReset = () => {
         setLapCount(0);
       };
 
+      //format!
     return(
         <div>
         <h2>{name}</h2>
